69275652c5f4f924efc949f95feeaac62fbc16ca
BelalBassem <bbassem2005@gmail.com>
2026-09-08 16:58:58 +0300

    Add .gitignore

--- Order.Api/obj/Debug/net8.0/Order.Api.AssemblyInfo.cs
+++ Order.Api/obj/Debug/net8.0/Order.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c1bb9ef80b3b6260e16e95f2291fbbda286f92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b4be955f18f2b4b7e5d994bfe3d1020cb7126b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
